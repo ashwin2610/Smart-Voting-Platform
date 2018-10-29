4cac52c324c55ef2928ccc6ca469f757b8f8b567
Ashwin Nair <ashwinnair2610@gmail.com>
2018-10-29 14:45:51 +0530

    Commit

--- SmartVotingPlatform/vote/urls.py
+++ SmartVotingPlatform/vote/urls.py
@@ -1,9 +1,12 @@
 from . import views
 from django.urls import path, include
+from django.views.generic import TemplateView
 from django.contrib.auth import views as auth_views
 
 urlpatterns = [
 	path('', views.redirect, name='vote'),
 	path('parties/', views.party, name='parties'),
-	path('candidates/', views.candidate, name='candidates')
+	path('candidates/', views.candidate, name='candidates'),
+	path('success/', TemplateView.as_view(template_name="vote_success.html"), name='success'),
+	path('failure/', TemplateView.as_view(template_name="vote_fail.html"), name='failure'),
 ]
